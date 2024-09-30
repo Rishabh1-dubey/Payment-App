fee70ffeffcf3335ec109a4683e539114f2c96e2
Rishabh Dubey <rishabhdubey006@gmail.com>
2024-09-30 21:43:01 +0530

    adding signin and sign up pages

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { Heading } from "../components/Heading";
+import { SubHeading } from "../components/SubHeading";
+import { Input } from "../components/Input";
+import { ButtonSignIn } from "../components/ButtonSignIn";
+import { Bottom } from "../components/Bottom";
 
 const Signin = () => {
   return (
-    <div>Signin</div>
-  )
-}
+    <div className="bg-slate-200 h-screen flex justify-center">
+      <div className="flex flex-col justify-center">
+        <div className="rounded-lg bg-white w-80 text-center p-2 h-max- px-4">
+          <Heading label={"Sign up"} />
+          <SubHeading label={"Enter you Information to create new Acccount"} />
+          <Input label={"Email"} placeholder={"Ram1008@gmail.com"} />
+          <Input label={"FirstName"} placeholder={" 12345678"} />
+          <div className="pt-5 m-2">
+            <ButtonSignIn label={"Sign up"} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Signin
+export default Signin;
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,17 +1,33 @@
-import React from 'react'
-import { Heading } from '../components/Heading'
-import { SubHeading } from '../components/SubHeading'
+import React from "react";
+import { Heading } from "../components/Heading";
+import { SubHeading } from "../components/SubHeading";
+import { Input } from "../components/Input";
+import { ButtonSignIn } from "../components/ButtonSignIn";
+import { Bottom } from "../components/Bottom";
 
 const Signup = () => {
   return (
-    <div className='bg-slate-200 h-[500px] w-[400px] mt-16  py-9 mx-auto border border-red-300'>
-      <div>
-
-      <Heading label={"SignIn"}/>
-      <SubHeading label={"Enter you Information to create new Acccount"}/>
+    <div className="bg-slate-200 h-screen flex justify-center">
+      <div className="flex flex-col justify-center">
+        <div className="rounded-lg bg-white w-80 text-center p-2 h-max- px-4">
+          <Heading label={"Sign up"} />
+          <SubHeading label={"Enter you Information to create new Acccount"} />
+          <Input label={"FirstName"} placeholder={" Enter your FirstName"} />
+          <Input label={"Lastname"} placeholder={" Enter your lastName"} />
+          <Input label={"Email"} placeholder={"Ram1008@gmail.com"} />
+          <Input label={"firstName"} placeholder={" 12345678"} />
+          <div className="pt-5 m-2">
+            <ButtonSignIn label={"Sign up"} />
+          </div>
+          <Bottom
+            label={"Already have an account?"}
+            buttonText={"Sign in"}
+            to={"/signin"}
+          />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
