f03914a08f6be16ea74fde26915b4842b908dfa8
Rishabh Dubey <rishabhdubey006@gmail.com>
2024-12-03 21:57:28 +0530

    adding router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,12 +6,14 @@ import Dashboard from './pages/Dashboard'
 import Send from './pages/Send'
 
 function App() {
+  
 
   return (
   
       <>
         <BrowserRouter>
         <Routes>
+          <Route path='/' element={<Signin/>}/>
           <Route path='/signin' element={<Signin/>}/>
           <Route path='/signup' element={<Signup/>}/>
           <Route path='/dashboard' element={<Dashboard/>}/>
---
+++ frontend/src/constent.js
@@ -0,0 +1,1 @@
+export const IMG_LOGO="https://static.vecteezy.com/system/resources/thumbnails/004/999/766/small_2x/digital-wallet-logo-design-template-with-pixel-effect-logo-concept-of-credit-card-crypto-wallet-fast-online-payment-vector.jpg"
