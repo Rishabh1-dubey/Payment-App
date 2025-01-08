ae3a5155da73e4c249a1c428617f9b62ee337db2
Rishabh Dubey <rishabhdubey006@gmail.com>
2025-01-08 14:23:05 +0530

    adding redux into our app

---
+++ frontend/src/redux/userSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+const userSlice = createSlice({
+  name: "userName",
+  initialState: {
+    user: null,
+  },
+  reducers: {
+    SetUser: (state, action) => {
+      state.user = action.payload;
+    },
+  },
+});
+
+export const { SetUser } = userSlice.actions;
+export default userSlice.reducer;
